af1bff601b167c248e58383fd072eacce64ae7a2
felipedeluque-ui <felipe.deluque@escola.pr.gov.br>
2026-03-25 08:46:18 -0300

    Create desafio1.py

---
+++ desafio1.py
@@ -0,0 +1,9 @@
+# Código funcional: Soma de dois números
+# Entrada do primeiro número pelo usuário
+num1 = float(input("Digite o primeiro número: "))
+# Entrada do segundo número pelo usuário
+num2 = float(input("Digite o segundo número: "))
+# Cálculo da soma dos dois números
+soma = num1 + num2
+# Saída do resultado na tela
+print("A soma de", num1, "e", num2, "é igual a", soma)
