5e3bb69f394b00c5b49e151c9259fb68bd9b13e0
felipedeluque-ui <felipe.deluque@escola.pr.gov.br>
2026-03-25 19:24:52 -0300

    desafio2.py

--- desafio2.py
+++ desafio2.py
@@ -1,1 +1,8 @@
 # Codigo Funcional; Analisar se o numero é impar ou par
+#analisar numero escolhido pelo participante
+num1 = float(input("Digite o o numero escolhido: "))
+#confirmação se o numero é impar ou par
+se numero % 2 == 0:
+ print(f"{numero} é par.")
+se nao
+rint(f"{numero} é ímpar.")
